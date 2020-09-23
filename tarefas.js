39351895a79283c653401cad07e6e5a89877c8c8
Diego <diego.scc@hotmail.com>
2020-09-22 21:35:02 -0300

    Aula 22/09

---
+++ tarefas.js
@@ -0,0 +1,44 @@
+var botaoAdicionar = document.getElementById('adicionar');
+var inputTarefa = document.getElementById('tarefa');
+
+inputTarefa.onkeypress = function(evento){
+    if(evento.key == 'Enter'){
+        adicionar();
+    }
+}
+
+    botaoAdicionar.addEventListener('click', adicionar);
+
+    function adicionar(){
+        var tarefa = inputTarefa.value;
+
+    if(tarefa !== ''){
+        var li = document.createElement('li');
+        li.innerText = tarefa;
+
+//        li.style.color = 'red';
+//
+//        li.onclick = function(){
+//          li.style.color = 'green';
+//        }
+//        
+//        li.ondblclick = function(){
+//            li.style.color = 'red';
+//        }
+
+        li.classList.add('para-fazer');
+        li.onclick = function(){
+            li.classList.remove('para-fazer');
+            li.classList.add('concluidas');
+        }
+        li.ondblclick = function(){
+            li.classList.remove('concluidas');
+            li.classList.add('para-fazer');
+        }
+
+        var ol = document.getElementById('tarefas');
+        ol.appendChild(li);
+        
+        inputTarefa.value = '';
+    }
+}
